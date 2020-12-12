20a7452efe2ad09e4915f570805af68c385f197e
ryo.kanesima <ryo.kanesima58@gmail.com>
2020-12-12 09:57:00 +0000

    user signup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 (1..100).each do |number|
-  Task.create(status: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
+  Task.create(status: 'title ' + number.to_s, content: 'test content ' + number.to_s)
 end
